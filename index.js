8b8529549f97e3c473497acbeef84594c09a6517
Saad Darbari <saaddarbari318@gmail.com>
2024-04-15 00:29:25 +0500

    push my code on Github

---
+++ index.js
@@ -0,0 +1,33 @@
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 83.61,
+    PKR: 277.3,
+};
+let userAns = await inquirer.prompt([
+    {
+        name: 'from',
+        message: "Enter From currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: 'to',
+        message: "Enter to currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+    },
+    {
+        name: "amount",
+        type: "number",
+        message: "Enter your Amount"
+    },
+]);
+let fromAmount = currency[userAns.from];
+let toAmount = currency[userAns.to];
+let amount = userAns.amount;
+let baseAmount = amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
